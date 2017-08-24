e269d0976bd59e3d4d7bddf6f200287530f9989b
ASUS-KD\Admin <delovski.office@gmail.com>
2017-08-25 00:05:15 +0200

    new progress updated

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -10,14 +10,7 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   closeModal() {
-    // var findModal;
-    // findModal=document.getElementsByClassName('modal');
-    // findModal.removeClass('show');
-
     document.getElementById("myModal").classList.remove("show");
-
-    // findModal.removeClass("show");
-    // console.log(findModal);
   }
 
   ngOnInit() {
