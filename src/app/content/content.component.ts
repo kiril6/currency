a1b89c224aaaaef04ccbb9f82fb727eac7035647
MUSALA\kiril.delovski <kiril.delovski@musala.com>
2017-11-02 16:29:04 +0100

    form sending fixed, email can be sent

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,17 +8,22 @@ import { AppService, IMessage } from './app.service';
 })
 export class ContentComponent implements OnInit {
 
+  public success:boolean;
+
 
   message: IMessage = {};
-
 
   constructor(private appService: AppService) { }
 
 
   sendEmail(message: IMessage) {
     this.appService.sendEmail(message).subscribe(res => {
+      this.success=true;
+      alert('The form has been sent!');
       console.log('AppComponent Success', res);
     }, error => {
+      this.success=false;
+      alert('The form was not sent!/n Please refresh and try again.');
       console.log('AppComponent Error', error);
     })
   }
--- src/assets/mailer.php
+++ src/assets/mailer.php
@@ -1,78 +1,51 @@
 ﻿<?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
 
-    // Only process POST reqeusts.
-		$a = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
-		$countrycode= $a['geoplugin_countryCode'];
-	
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-		$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+header('Content-type: application/json');
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }	
+$errors = '';
+// $to_email="delovski.office@gmail.com";
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "intercoop.delovski@hotmail.com";
+if(empty($errors))
+{
 
-        // Set the email subject.
-        $subject = "Intercoop website from: $name";
+	$postdata = file_get_contents("php://input");
+	$request = json_decode($postdata);
 
-        // Build the email content.
-		
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n\n";
-		$email_content .= "IpAddress:\n" .$_SERVER['REMOTE_ADDR'] . "\n";	
+	$from_email = $request->email;
+	$message = $request->message;
+	$from_name = $request->name;
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-		//$email_respond = "Your email has been received|";
-		$message = "\r\n";
+  // $to_email = $from_email;
+  $to_email="delovski.office@gmail.com";
 
-		$message .="Thank you for contacting!
+	$contact = "<p><strong>Name:</strong> $from_name</p>
+							<p><strong>Email:</strong> $from_email</p>";
+	$content = "<p>$message</p>";
 
-Greetings,
-intercoop
+	$website = 'intercoop';
+	$email_subject = "$website: New email $from_name sent";
 
-Email: intercoop.delovski.net
-Website: http://intercoop.delovski.net";
-	
-		$_SERVER['REMOTE_ADDR'];
-        // Send the email.
-		
-		if ($countrycode=='RU' || $countrycode=='UA'){
-			header( 'Location: http://google.com' ) ;
-		}else{
-		
-        if (mail($recipient, $subject, $email_content, $email_headers, !$country)) {
-            // Set a 200 (okay) response code.
-          // http_response_code(200);			   			
-           // echo "Thank you. The message has been sent.";			
-		
-				mail("$email", "$name, $message");
-			
-        } else {
-            // Set a 500 (internal server error) response code.
-           // http_response_code(500);
-           // echo "Error! Your message was not sent!";
-        }
-	}
+	$email_body = '<html><body>';
+	$email_body .= "$contact $content";
+	$email_body .= '</body></html>';
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "Some error occured, try again.";
-    }
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$headers .= "From: $from_email\n";
+	$headers .= "Reply-To: $from_email";
+
+	mail($to_email,$email_subject,$email_body,$headers);
+
+	$response_array['status'] = 'success';
+	$response_array['from'] = $from_email;
+	echo json_encode($response_array);
+	echo json_encode($from_email);
+	header($response_array);
+	return $from_email;
+} else {
+	$response_array['status'] = 'error';
+	echo json_encode($response_array);
+  // header('Location: http://google.com');
+  alert("error occured! Reload the page and try again.");
+}
 ?>
